69c3cea36bc9dda17cb431a94c6e6800e9fb043c
tr1ck <meyers.ricky@gmail.com>
2016-05-17 02:39:23 +0000

    added gram detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Grammable::Application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create]
+  resources :grams, only: [:new, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
