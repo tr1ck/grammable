23e3da3c27eee9bc660c9232df7b510301d2cb2e
tr1ck <meyers.ricky@gmail.com>
2016-05-20 01:19:43 +0000

    Styled app

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,6 @@
 class Gram < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   validates :message, presence: true
   validates :picture, presence: true
   mount_uploader :picture, PictureUploader
